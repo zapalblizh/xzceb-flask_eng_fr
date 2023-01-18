8cfcbcc39d075ce4fd553712a8ef1afb8479738a
zapalblizh <zapal.blizh@gmail.com>
2023-01-18 07:56:47 -0500

    Solved issue using virtualenv

--- final_project/server.py
+++ final_project/server.py
@@ -7,18 +7,18 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to French"
+    translatedtext = translator.englishToFrench(textToTranslate)
+    return "Translated text to French: %s" %(translatedtext)
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    # Write your code here
-    return "Translated text to English"
+    translatedtext = translator.frenchToEnglish(textToTranslate)
+    return "Translated text to English: %s" %(translatedtext)
 
 @app.route("/")
 def renderIndexPage():
-    # Write the code to render template
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
